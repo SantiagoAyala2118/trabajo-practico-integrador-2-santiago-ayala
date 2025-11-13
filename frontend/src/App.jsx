29514b764054dad6a4d1d294d915f03e6128a037
Santiagio Ayala <santitomayala07@gmail.com>
2025-11-12 23:03:49 -0300

    Lógica para hacer el logout añadida

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router";
 import { AppRouter } from "./router/AppRouter";
 
 function App() {
-  return <AppRouter />;
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const logoutFetch = await fetch("http://localhost:3000/api/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+
+      if (!logoutFetch.ok) {
+        console.log("Error al desloguearse");
+      }
+
+      navigate("/login");
+    } catch (err) {
+      return console.log("Error durante el logout", err);
+    }
+  };
+
+  return <AppRouter onLogout={handleLogout} />;
 }
 
 export default App;
